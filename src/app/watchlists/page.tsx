323bca91decf5008762634900b3f369c6a8d421c
Raj <blcc.raj@gmail.com>
2023-12-27 14:52:37 +0530

    refactor: WatchList component to improve code readability

--- src/app/watchlists/page.tsx
+++ src/app/watchlists/page.tsx
@@ -3,20 +3,12 @@ import PostCard from "../component/PostCard"
 import { UserButton } from "@clerk/nextjs"
 import { auth, currentUser } from "@clerk/nextjs";
 
-
 const WatchList = async () => {
-    const getWatchList = async () => {
-        const response = await db.watchlist.findMany({
-            include: {
-                items: true,
-            },
-        })
-        return response
-    }
-    const user = await currentUser()
-    const { userId } = auth()
-    const watchlist = await getWatchList()
-    console.log(user)
+    const watchlist = await db.watchlist.findMany({
+        include: {
+            items: true
+        }
+    })
     return (
         <section>
             <div className="grid place-content-center md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10 px-10">
